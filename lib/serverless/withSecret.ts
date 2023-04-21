15bc3caaa266194c1ac69cb4096be9d423634027
Julien Barbay <julien.barbay@gmail.com>
2023-04-21 16:02:16 +0700

    fix: withSecret middleware

--- lib/serverless/withSecret.ts
+++ lib/serverless/withSecret.ts
@@ -4,10 +4,10 @@ export function withSecret(req: VercelRequest, res: VercelResponse) {
   const api_secret = process?.env.API_SECRET?.trim() ?? ''
 
   if (!api_secret.length) {
-    return res.status(500).end('Unprotected endpoint')
+    res.status(500).end('Unprotected endpoint')
   }
 
   if (req.headers['authorization'] !== `Bearer ${api_secret}`) {
-    return res.status(401).end('Unauthorized')
+    res.status(401).end('Unauthorized')
   }
 }
